f44a922a2ce95e9f60ddd6d882b94c41233b6c88
Julián David <jdtorregrosas@gmail.com>
2017-02-09 23:21:58 -0500

    Create templating views and add basic routing

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-const m = require('../app/matrix');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { output: ''});
+});
+app.post('/signup', function(req,res){
+  console.log(req.body.input);
+  res.render('index', { output: req.body.input});
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+app.use(express.static('public'));
